84bded8c4c563cd50f49d540e9fc5674e6f52d1d
ErcinDedeoglu <e.dedeoglu@gmail.com>
2025-02-08 15:22:53 +0400

    build: add Docker build and run commands to script for testing
    refactor(Dockerfile): simplify Python installation by using python3-full
    
    refactor(Dockerfile): remove unnecessary python3-venv and related setup
    
    chore(Dockerfile): update pip install command to use pip3 with --break-system-packages

--- .sh
+++ .sh
@@ -3,3 +3,10 @@ repomix --no-file-summary --no-security-check --include "src/**,.github/**" --ou
 repomix --no-file-summary --no-security-check --include ".github/**" --output "repopack.yml"
 
 repomix --no-file-summary --no-security-check --include "src/**" --output "repopack.yml"
+
+
+docker build -f src/Dockerfile -t test src && docker run --rm -it test
+
+docker run -it --rm \
+  --entrypoint sh \
+  test
