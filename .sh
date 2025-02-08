784488714aa06d485f8604d90966a179d391f54a
ErcinDedeoglu <e.dedeoglu@gmail.com>
2025-02-08 14:12:31 +0400

    build: add docker-compose.yml for GitHub runner service setup
    chore: replace cmd file with .sh script for repomix command execution

---
+++ .sh
@@ -0,0 +1,5 @@
+repomix --no-file-summary --no-security-check --include "src/**,.github/**" --output "repopack.yml"
+
+repomix --no-file-summary --no-security-check --include ".github/**" --output "repopack.yml"
+
+repomix --no-file-summary --no-security-check --include "src/**" --output "repopack.yml"
--- cmd
+++
@@ -1,3 +0,0 @@
-repomix --include "src/**,.github/**" --output "repopack.yml"
-
-repomix --include ".github/**" --output "repopack.yml"
